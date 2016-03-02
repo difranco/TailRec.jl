5d1b428b487817bcec158563e63a02cb1a93c7c2
Takekazu KATO <takekazu.kato@gmail.com>
2016-03-02 09:23:43 +0900

    initial commit

--- README.md
+++ README.md
@@ -1,3 +1,28 @@
 # TailRec
 
 [![Build Status](https://travis-ci.org/TakekazuKATO/TailRec.jl.svg?branch=master)](https://travis-ci.org/TakekazuKATO/TailRec.jl)
+
+# 説明
+
+末尾再帰関数をループに書き換えて最適化します．内部では@label, @gotoマクロを使って単純に末尾再帰呼び出しを@gotoでループにします．
+
+相互再帰呼び出しには対応していません．
+
+# 使い方
+
+```jl
+using TailRec
+@tailrec factorial(x,i)
+if x == 1
+i
+else
+factorial(x-1,i*x)
+end
+```
+
+```jl
+@tailrec factorial(x,i)=x==1?i:factorial(x-1,i*x)
+
+factorial(BigInt(100),1)
+93326215443944152681699238856266700490715968264381621468592963895217599993229915608941463976156518286253697920827223758251185210916864000000000000000000000000
+```
--- src/TailRec.jl
+++ src/TailRec.jl
@@ -1,5 +1,34 @@
 module TailRec
+export @tailrec
 
-# package code goes here
+macro tailrec(func)
+    fargs=map(e->isa(e,Expr)?e.args[1]:e,func.args[1].args)
+    fbody=func.args[2]
+    fbody=rewrite(fbody,fargs)
+    func.args[2]=Expr(:block,:(@label retry),fbody)
+    esc(func)
+end
 
-end # module
+function rewrite(expr,args,callflag=false)
+    if !isa(expr,Expr)
+        expr
+    elseif expr.head == :call && expr.args[1] == args[1]
+        if callflag
+            warn("Not tail recursive call is found.")
+            expr
+        else
+            newargs=Expr(:tuple)
+            newargs.args=args[2:end]
+            oldargs=Expr(:tuple)
+            oldargs.args=expr.args[2:end]
+            Expr(:block, Expr(:(=),newargs,oldargs), :(@goto retry) )
+        end
+    elseif expr.head == :block
+        expr.args[end]=rewrite(expr.args[end],args,expr.head==:call? true:callflag)
+        expr
+    else
+        expr.args = map(a->rewrite(a,args,expr.head==:call? true:callflag),expr.args)
+        expr
+    end
+end
+end
